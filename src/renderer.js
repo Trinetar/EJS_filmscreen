c13b5f9ce1e85896707633736c96c5a07384b2fa
Trinetar <proftd16@gmail.com>
2024-04-04 12:18:25 +0530

    Updated version: V2

--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
 
+// Define a map to store image descriptions
+const imageDescriptions = {
+  'image1': 'Dune Part 1, Realsed in 2021, earned half a billon over OTT',
+  'image2': 'Dune Part 2, Released in 2024, Hoping to earn a billion',
+  'image3': 'Oppenheimer, Released in 2023, Story of R.Oppenheimer man who changed the world'
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const sidebar = document.getElementById('sidebar');
   const imageDescription = document.getElementById('image-description');
@@ -16,7 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
       img.src = `../images/${file}`;
       img.onclick = () => {
         const imageName = file.split('.')[0];
-        imageDescription.textContent = `Description for ${imageName}`;
+        // Get the description from the map
+        const description = imageDescriptions[imageName] || 'No description available';
+        imageDescription.textContent = description;
       };
       sidebar.appendChild(img);
     });
